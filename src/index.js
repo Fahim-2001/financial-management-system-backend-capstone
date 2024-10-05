0199752bd1ce87d18589f6a65513eb7b96732fbc
Fahim-2001 <mdyasirofficial2001@gmail.com>
2024-10-05 15:23:48 +0600

    Project Setup Done

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+require("dotenv").config();
+require("colors");
+const PORT = process.env.PORT || 8000;
+
+var corsOptions = {
+    origin: "*",
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Accept",
+        "x-reset-token",
+        "x-invite-token",
+        "x-api-key",
+        "x-www-form-urlencoded",
+    ],
+    credentials: true,
+};
+
+// MIDDLEWARES
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    res.status(200).json({
+        message: "Welcome to The Valor's Financial Management System Backend Service.",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`.green.bold);
+});
