e7093315ecf143468ac0f3cf28aced64524fed96
Fahim-2001 <mdyasirofficial2001@gmail.com>
2025-05-03 14:19:20 +0600

    chore: fixed facilities issue in Services end-point

--- src/modules/fin_services/fin_services.route.js
+++ src/modules/fin_services/fin_services.route.js
@@ -2,11 +2,10 @@ const express = require("express");
 const finServicesRouter = express.Router();
 const serviceController = require("./fin_services.controller");
 
-// Routes for CRUD operations
-finServicesRouter.get("/", serviceController.getAllServices); // Get all services
-finServicesRouter.get("/:id", serviceController.getServiceById); // Get a service by ID
-finServicesRouter.post("/", serviceController.createService); // Create a new service
-finServicesRouter.put("/:id", serviceController.updateServiceById); // Update an existing service
-finServicesRouter.delete("/:id", serviceController.deleteService); // Delete a service
+finServicesRouter.get("/", serviceController.getAllServices); 
+finServicesRouter.get("/:id", serviceController.getServiceById);
+finServicesRouter.post("/", serviceController.createService); 
+finServicesRouter.put("/:id", serviceController.updateServiceById); 
+finServicesRouter.delete("/:id", serviceController.deleteService);
 
 module.exports = finServicesRouter;
--- src/modules/fin_services/fin_services.services.js
+++ src/modules/fin_services/fin_services.services.js
@@ -5,32 +5,29 @@ const {
 } = require("../../utils/findSmallestAvailableId");
 const prisma = require("../../config/prisma.config");
 
-// Service to get all services
 const getAllServices = async () => {
     return await prisma.service.findMany({ orderBy: { id: "asc" } });
 };
 
-// Service to get a service by ID
 const getServiceById = async (id) => {
     return await prisma.service.findUnique({
         where: { id },
     });
 };
 
-// Service to create a new service
 const createService = async (data) => {
     const { title, price, type, facilities, session_type, userId } = data;
     const date = generateTimestamp();
 
     const missingId = await findSmallestAvailableId("service");
-
+    const faci = facilities.toString();
     return await prisma.service.create({
         data: {
             id: missingId,
             title,
             price,
             type,
-            facilities,
+            facilities: faci,
             session_type,
             userId,
             created_at: date,
@@ -39,7 +36,6 @@ const createService = async (data) => {
     });
 };
 
-// Service to update an existing service
 const updateService = async (id, data) => {
     const { title, price, type, facilities, session_type, userId } = data;
     const dateOfUpdate = generateTimestamp();
@@ -57,7 +53,6 @@ const updateService = async (id, data) => {
     });
 };
 
-// Service to delete a service
 const deleteService = async (id) => {
     return await prisma.service.delete({
         where: { id },
