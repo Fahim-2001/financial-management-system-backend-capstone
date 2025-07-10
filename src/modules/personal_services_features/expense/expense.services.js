d2ea6c97079cbff10655456f5b9bbdaf65b319a1
Fahim-2001 <mdyasirofficial2001@gmail.com>
2025-07-11 00:24:47 +0600

    feat: updated expense method into class

--- src/modules/personal_services_features/expense/expense.services.js
+++ src/modules/personal_services_features/expense/expense.services.js
@@ -1,87 +1,87 @@
 const { generateTimestamp } = require("../../../utils/generativeFunctions");
-const {
-    timestampToMilliseconds,
-    convertDateFormat,
-} = require("../../../utils/timestampToMS");
-const {
-    findSmallestAvailableId,
-} = require("../../../utils/findSmallestAvailableId");
+const { timestampToMilliseconds, convertDateFormat } = require("../../../utils/timestampToMS");
+const { findSmallestAvailableId } = require("../../../utils/findSmallestAvailableId");
 const prisma = require("../../../config/prisma.config");
-exports.createExpenseIntoDB = async (data = Object) => {
-    try {
-        const missingId = await findSmallestAvailableId("expense");
-        const date = generateTimestamp();
-        const userGivenDate = convertDateFormat(data.date);
-        return prisma.expense.create({
-            data: {
-                id: missingId,
-                title: data.title,
-                amount: parseFloat(data.amount),
-                category: data.category,
-                date: userGivenDate,
-                created_at: date,
-                updated_at: date,
-                user_id: parseInt(data.user_id),
-            },
-        });
-    } catch (error) {
-        throw new Error(error);
-    }
-};
 
-exports.getAllExpenses = async () => {
-    try {
-        return await prisma.expense.findMany({
-            orderBy: { date: "desc" },
-        });
-    } catch (error) {
-        throw new Error(error);
+class ExpenseService {
+    static async createExpenseIntoDB(data = {}) {
+        try {
+            const missingId = await findSmallestAvailableId("expense");
+            const date = generateTimestamp();
+            const userGivenDate = convertDateFormat(data.date);
+            return prisma.expense.create({
+                data: {
+                    id: missingId,
+                    title: data.title,
+                    amount: parseFloat(data.amount),
+                    category: data.category,
+                    date: userGivenDate,
+                    created_at: date,
+                    updated_at: date,
+                    user_id: parseInt(data.user_id),
+                },
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
     }
-};
 
-exports.getAllExpensesByUserId = async (userId = Number, expenses = Array) => {
-    try {
-        const data = expenses?.filter((expense) => {
-            return expense?.user_id == userId;
-        });
-        return data;
-    } catch (error) {
-        throw new Error(error);
+    static async getAllExpenses() {
+        try {
+            return await prisma.expense.findMany({
+                orderBy: { date: "desc" },
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
     }
-};
 
-exports.getExpenseByIdFromDB = async (id = Number) => {
-    try {
-        return prisma.expense.findUnique({ where: { id } });
-    } catch (error) {
-        throw new Error(error);
+    static async getAllExpensesByUserId(userId = Number, expenses = Array) {
+        try {
+            const data = expenses?.filter((expense) => {
+                return expense?.user_id == userId;
+            });
+            return data;
+        } catch (error) {
+            throw new Error(error);
+        }
     }
-};
 
-exports.updateExpenseInDB = async (id = Number, data = Object) => {
-    try {
-        const date = generateTimestamp();
-        const userGivenDate = convertDateFormat(data.date);
-        return prisma.expense.update({
-            where: { id },
-            data: {
-                title: data.title,
-                amount: parseFloat(data.amount),
-                category: data.category,
-                date: userGivenDate,
-                updated_at: date,
-                user_id: data.user_id,
-            },
-        });
-    } catch (error) {
-        throw new Error(error);
+    static async getExpenseByIdFromDB(id = Number) {
+        try {
+            return prisma.expense.findUnique({ where: { id } });
+        } catch (error) {
+            throw new Error(error);
+        }
     }
-};
 
-exports.deleteExpenseFromDB = async (id = Number) => {
-    try {
-        return prisma.expense.delete({ where: { id } });
-    } catch (error) {
-        throw new Error(error);
+    static async updateExpenseInDB(id = Number, data = {}) {
+        try {
+            const date = generateTimestamp();
+            const userGivenDate = convertDateFormat(data.date);
+            return prisma.expense.update({
+                where: { id },
+                data: {
+                    title: data.title,
+                    amount: parseFloat(data.amount),
+                    category: data.category,
+                    date: userGivenDate,
+                    updated_at: date,
+                    user_id: data.user_id,
+                },
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
     }
-};
+
+    static async deleteExpenseFromDB(id = Number) {
+        try {
+            return prisma.expense.delete({ where: { id } });
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+}
+
+module.exports = ExpenseService;
--- src/modules/personal_services_features/income/income.route.js
+++ src/modules/personal_services_features/income/income.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const incomeController = require('./income.controller');
+const IncomeController = require('./income.controller');
 
-router.post('/', incomeController.createIncome);
-router.get('/', incomeController.getAllIncomes);
-router.get('/:id', incomeController.getIncomeById);
-router.put('/:id', incomeController.updateIncome);
-router.delete('/:id', incomeController.deleteIncome);
+router.post('/', IncomeController.createIncome);
+router.get('/user/:id', IncomeController.getAllIncomes);
+router.get('/:id', IncomeController.getIncomeById);
+router.put('/:id', IncomeController.updateIncome);
+router.delete('/:id', IncomeController.deleteIncome);
 
 module.exports = router;
